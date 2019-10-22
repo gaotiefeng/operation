d10867511255b0b2406db8cc57fcc65347cf6062
gaotiefeng <1096392101@qq.com>
2019-10-22 12:42:28 +0800

    update

---
+++ src/stringOperation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace feng\redisOperation;
+
+abstract class stringOperation
+{
+
+    protected $prefix;
+    /**
+     * redis
+     * @return mixed
+     */
+    abstract public function redis();
+
+    public function setString($key,$val)
+    {
+        $this->redis()->set($this->prefix.$key,$val);
+    }
+
+    public function getString($key)
+    {
+        return $this->redis()->get($this->prefix.$key);
+    }
+
+    public function del($key)
+    {
+        $this->redis()->del($this->prefix.$key);
+    }
+}
